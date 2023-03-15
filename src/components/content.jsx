1f54e1555d6870742f50a2f0e86e7743544a5fbd
Ganeshkumar <123441945+GaneshTRL748@users.noreply.github.com>
2023-03-15 18:36:48 +0530

    Add files via upload

---
+++ src/components/content.jsx
@@ -0,0 +1,36 @@
+import React,{useState} from "react";
+import '../styles/content.css'
+import Cards from './cards'
+let Content=({additem,list,cartbtnoperation})=>
+{
+    const [data,Setdata]=useState(list);
+   const filterItem=(value)=>
+   {
+           const result=list.filter((curdata)=>
+           {
+              return curdata.cat===value;
+           }
+           );
+           Setdata(result);
+   }
+  
+    return (
+        <div className="content">
+            <div className="category">
+                <div><button onClick={()=>Setdata(list)}>All</button></div>
+               <div><button onClick={()=>filterItem('vegetables')}>Vegetables</button></div> 
+               <div><button onClick={()=>filterItem('fruits')}>Fruits</button></div> 
+               <div><button onClick={()=>filterItem('snacks')}>Snacks</button></div> 
+            </div>
+            <div className="content_cards">
+                   {
+                        data.map((item)=>(
+                            <Cards data={item} additem={additem} key={item.id} cartbtnoperation={cartbtnoperation}/>
+                        ))
+                    }   
+             </div>
+        </div>
+
+    )
+}
+export default Content
